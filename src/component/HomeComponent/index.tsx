ab2ed70726c145cd169f40de44d3a3a8be5c5af8
Goyal_ Anuj (anujgoyal4@deloitte.com) <anujgoyal4@your.hostname.com>
2022-02-10 20:20:12 +0530

    featured-card

--- src/component/HomeComponent/index.tsx
+++ src/component/HomeComponent/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./index.css";
+import BMWX3 from "../../assets/images/BMW X3.png";
+import BMWX5 from "../../assets/images/BMW X5.png";
+import BMWX6 from "../../assets/images/BMW X6.png";
+import BMWX7 from "../../assets/images/BMW X7.png";
+import unionArrow from "../../assets/images/arrow.png";
 
 interface IProps {
   name: string;
@@ -27,6 +32,51 @@ const HomeComponent = (props: IProps) => {
           <button className="tablinks" id="defaultOpen">Popular</button>
           <button className="tablinks">Just launched</button>
           <button className="tablinks">Upcoming</button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img className="car-image" src={BMWX3} alt="BMW X3" />
+            <div className="container">
+              <h2>BMW X3</h2>
+              <p className="title">69.9 lakh onwards</p>
+              <p><button className="button"><img src={unionArrow} /></button></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img className="car-image" src={BMWX5} alt="BMWX5" />
+            <div className="container">
+              <h2>BMW X5</h2>
+              <p className="title">76.5 lakh onwards</p>
+              <p><button className="button"><img src={unionArrow} /></button></p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="column">
+          <div className="card">
+            <img className="car-image" src={BMWX6} alt="BMWX6" />
+            <div className="container">
+              <h2>BMW X6</h2>
+              <p className="title">1.02 crore onwards</p>
+              <p><button className="button"><img src={unionArrow} /></button></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img className="car-image" src={BMWX7} alt="BMWX7" />
+            <div className="container">
+              <h2>BMW X7</h2>
+              <p className="title">1.15 crore onwards</p>
+              <p><button className="button"><img src={unionArrow} /></button></p>
+            </div>
+          </div>
         </div>
       </div>
       
